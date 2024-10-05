d418eb7a8cc48ef8ea8399dbad3470fcda034de2
Mussie <mussieabraham03@gmail.com>
2024-10-05 15:26:59 -0400

    assignment 5

---
+++ mini_fb/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Profile(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    city = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    profile_image_url = models.URLField(max_length=200)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
